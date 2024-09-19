50b74a5ffcc7b27fa146adc96f7c757e41c4d971
math-antero <anteromath@gmail.com>
2024-09-18 23:48:45 -0300

    Tipos add

--- java-anatomia-classes/src/curso/math/primeirasemana/TiposVariaveis.java
+++ java-anatomia-classes/src/curso/math/primeirasemana/TiposVariaveis.java
@@ -2,4 +2,11 @@ package curso.math.primeirasemana;
 
 public class TiposVariaveis {
     
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        System.out.println(" " + (++numero));
+    }
+
 }
